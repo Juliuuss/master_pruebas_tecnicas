4d095dfa9be8038b5a0f93215e8ce469eb85c3ab
Julio Espinoza <j.esp.ver@gmail.com>
2023-05-30 00:32:15 -0400

    primer commit

---
+++ ejercicio1.js
@@ -0,0 +1,39 @@
+/*
+Advertencias:
+- En español por fines didacticos, en la vida real usa nombres en ingles.
+- Test en cada ejercicio, lo veremos al final del curso para ir al grano.
+- Ejercicios genéricos, puedes usar cualquier lenguaje.
+- Ejercicios nuevos y diferentes a los del Master en Lógica de Programación
+- Siempre mostrar el resultado en la consola / terminal.
+- Hay muchas soluciones validas para un mismo ejercicio.
+ 
+Enunciado Ejercicio 1:
+Dado un array o un objeto de superhéroes de Marvel, haz un programa que pueda
+mostrar la información de un superheroe.
+ 
+Y añade una capacidad de buscar la información de varios superhéroes a la vez.
+ 
+Ejemplo:
+mostrarInformacionSuperheroe('Iron Man');
+ 
+Salida:
+Nombre real: Tony Stark
+Poderes: Tecnología avanzada, Movilidad aérea
+Equipo: Los vengadores
+ 
+Ejemplo 2: 
+mostrarInformacionSuperheroes([array de nombres]]);
+ 
+Salida:
+Muestra la información de todos los superheroes
+ 
+*/
+
+// crear array de super heroes de marvel.
+const info_superheroes = {
+    'Iron Man' :{
+        nombre_real :'Tony Stark'
+    },
+    'Capitan America': {},
+
+}
